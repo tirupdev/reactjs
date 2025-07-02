4b1ffa9fbdf0bf2e271db19fb36aec51e7c2d59b
tirupdev <ulli.tirupataiah@gmailcom>
2025-07-02 14:02:32 +0530

    react gha-ec2self3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Hello from React on EC2! by tirup</h1>
+      <h1>Hello from React on EC2! by tirup2</h1>
       <p>This is a test deployment.</p>
     </div>
   );
